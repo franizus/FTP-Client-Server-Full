f0a9522ad4cb3df666641d474b37ed1950487748
Unknown <franizus@live.com>
2017-11-30 12:43:13 -0500

    added sql log capabilities to server

--- seq_client.py
+++ seq_client.py
@@ -2,21 +2,21 @@ import socket
 
 
 HOST = 'localhost'   # Symbolic name, meaning all available interfaces
-PORT = 8888 # Arbitrary non-privileged port
+PORT = 8889 # Arbitrary non-privileged port
 
 #create an INET, STREAMing socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #now connect to the web server on port 80
 # - the normal http port
-print 'connecting to ' + HOST + ':' + str(PORT)
+print('connecting to ' + HOST + ':' + str(PORT))
 s.connect((HOST, PORT))
 
 file = open("server_code_received.txt", "w")
 data = s.recv(128)
-while data != "file finished":
-    file.write(data)
+while data.decode('ascii') != "file finished":
+    file.write(data.decode('ascii'))
     data = s.recv(128)
 
 file.close()
-print 'closing socket'
+print('closing socket')
 s.close()
