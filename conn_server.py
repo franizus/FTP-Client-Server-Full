57e93150de49e64fb6dfff1905b2ec6c4fd0b32d
Unknown <franizus@live.com>
2017-12-11 10:52:37 -0500

    improved server

--- conn_server.py
+++ conn_server.py
@@ -4,24 +4,38 @@ import threading
 import select
 
 
-def broadcast_data(sock, message):
-    for socket in CONNECTION_LIST:
-        if socket != s and socket != sock:
-            try :
-                socket.send(message.encode('ascii'))
-            except :
-                socket.close()
-                CONNECTION_LIST.remove(socket)
+def broadcast_data(client, message):
+    for key in CONNECTION_DICT:
+        sock = CONNECTION_DICT.get(key)
+        if sock != s and sock != CONNECTION_DICT.get(client):
+            sock.send(message.encode('ascii'))
 
 
-def escucharCliente(cliente, direccion):
+def broadcast_connected_clients(socket):
+    for key in CONNECTION_DICT:
+        sock = CONNECTION_DICT.get(key)
+        if sock != s and sock != CONNECTION_DICT.get(client):
+            socket.send(('\r' + key + ' se encuentra en el chat\n').encode('ascii'))
+
+
+def send_data(sender, receiver, message):
+    if receiver in CONNECTION_DICT:
+        sock = CONNECTION_DICT.get(receiver)
+        if sock != s and sock != CONNECTION_DICT.get(client):
+            sock.send(message.encode('ascii'))
+    else:
+        sock = CONNECTION_DICT.get(sender)
+        sock.send((receiver + ' no conectado.\n').encode('ascii'))
+
+
+def listen_client(client, socket, direccion):
     size=1024
-    cliente.send('Bienvenido al Chat\n'.encode('ascii'))
+    socket.send(('Bienvenido al Chat ' + client + '\n').encode('ascii'))
+    broadcast_connected_clients(socket)
     while True:
-        data = cliente.recv(size).decode('ascii')
-        print(data + ' de ' + direccion[0] + ':' + str(direccion[1]))
+        data = socket.recv(size).decode('ascii')
         if data:
-            broadcast_data(cliente, "\r" + '<' + str(cliente.getpeername()) + '> ' + data)
+            broadcast_data(client, '\r<' + client + '> ' + data)
 
 
 if __name__ == "__main__":
@@ -29,6 +43,8 @@ if __name__ == "__main__":
     HOST = ''   # Symbolic name, meaning all available interfaces
     PORT = 8889 # Arbitrary non-privileged port
     CONNECTION_LIST = []
+    CONNECTION_DICT = {}
+    CLIENT_COUNTER = 0
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    
@@ -41,17 +57,21 @@ if __name__ == "__main__":
     
     threads = []
     #Start listening on socket
-    s.listen(10) 
+    s.listen(10)
     CONNECTION_LIST.append(s)
+    CONNECTION_DICT.update({'Server':s})
 
     #now keep talking with the client
     while 1:
-        read_sockets,ws,es = select.select(CONNECTION_LIST,[],[])
+        read_sockets,ws,es = select.select(CONNECTION_LIST, [], [])
         for sock in read_sockets:
             if sock == s:
                 conn, addr = s.accept()
+                client = 'C' + str(CLIENT_COUNTER + 1)
                 CONNECTION_LIST.append(conn)
-                threads.append(threading.Thread(target = escucharCliente, args = (conn,addr, )).start())
-                broadcast_data(conn, "[%s:%s] entered room\n" % addr)
+                CONNECTION_DICT.update({client:conn})
+                threads.append(threading.Thread(target = listen_client, args = (client, conn, addr, )).start())
+                broadcast_data(client, '\r' + client + ' entro al chat\n')
+                CLIENT_COUNTER += 1
     
     s.close()
